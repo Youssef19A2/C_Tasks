6a6d69b9ec02ef0e16d1f28ed15a74877e48135a
Youssef19A2 <youssefahmed36567@gmail.com>
2026-09-23 01:14:15 +0300

    Add files via upload

---
+++ Lec1/Test_1.c
@@ -0,0 +1,47 @@
+/*=============================================================================
+/ @author :Yousifahmed  
+/ @date : 20-9-2026
+/ @version : 1.0.0
+/ @brief : This is a C Application creation 
+/============================================================================== 
+*/
+
+/* ************************** Includes Section Start ************************** */
+#include<stdio.h>
+/* ************************** Includes Section End   ************************** */
+
+/* ************************** Definition Section Start ************************ */
+#define User_PI 3.14159
+/* ************************** Definition Section End   ************************ */
+
+/* ************************** Global Variable Declaration Section Start ************************** */
+unsigned int x=10;
+/* ************************** Global Variable Declaration Section End   ************************** */
+
+/* ************************** Sub-Program  Declaration Section Start ***************************** */
+/* ************************** Sub-Program  Declaration Section End   ***************************** */
+  void print_ahmed(void);
+
+int main() 
+{
+    printf("Hello,World!\n");
+    print_ahmed();
+    return 0;
+}
+
+/* ************************** Sub-Program  Defination Section Start ***************************** */
+void print_ahmed(void)
+{
+    printf("Hello Ahmed\n");
+}
+/* ************************** Sub-Program  Defination Section End   ***************************** */
+
+
+
+
+
+/*================================================================================================
+/ User                                Date                       Breif
+/=================================================================================================
+/ Yousif ahmed                     22Sep2026                    Initial Creation
+*/
