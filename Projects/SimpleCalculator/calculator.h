a9cab2486e23bf0714da2b926d2d39655f84a5e0
Youssef19A2 <youssefahmed36567@gmail.com>
2026-09-24 16:12:06 +0300

    Delete Projects/SimpleCalculator directory

--- Projects/SimpleCalculator/calculator.h
+++
@@ -1,6 +0,0 @@
-unsigned int GetSumming( int NumberOne ,  int NumberTwo);
-unsigned int GetSubStract( int NumberOne,  int NumberTwo);
-unsigned int GetMultiply( int NumberOne,  int NumberTwo);
-unsigned int GetModulus( int NumberOne,  int NumberTwo);
-unsigned int GetDivision( int NumberOne,  int NumberTwo);
-
